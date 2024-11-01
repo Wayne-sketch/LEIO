56a4c3b8f56e562a32600b9b32903d6a8fbf1e5d
ctx <cuitongxin201024@163.com>
2024-11-01 21:52:53 +0800

    补了update() 和 predict() 下一版改gtsam

--- LOAMGBA_Public_WS/src/LOAMGBA/src/LoopOptimization.cpp
+++ LOAMGBA_Public_WS/src/LOAMGBA/src/LoopOptimization.cpp
@@ -1351,6 +1351,61 @@ void processOdom(const nav_msgs::Odometry::ConstPtr &odom_msg, const std_msgs::H
     }
 }
 
+void predict(const sensor_msgs::ImuConstPtr &imu_msg)
+{
+    double t = imu_msg->header.stamp.toSec();
+    if (init_imu)
+    {
+        latest_time = t;
+        init_imu = 0;
+        return;
+    }
+    double dt = t - latest_time;
+    latest_time = t;
+
+    double dx = imu_msg->linear_acceleration.x;
+    double dy = imu_msg->linear_acceleration.y;
+    double dz = imu_msg->linear_acceleration.z;
+    Eigen::Vector3d linear_acceleration{dx, dy, dz};
+
+    double rx = imu_msg->angular_velocity.x;
+    double ry = imu_msg->angular_velocity.y;
+    double rz = imu_msg->angular_velocity.z;
+    Eigen::Vector3d angular_velocity{rx, ry, rz};
+
+    Eigen::Vector3d un_acc_0 = tmp_Q * (acc_0 - tmp_Ba) - estimator.g;
+
+    Eigen::Vector3d un_gyr = 0.5 * (gyr_0 + angular_velocity) - tmp_Bg;
+    tmp_Q = tmp_Q * Utility::deltaQ(un_gyr * dt);
+
+    Eigen::Vector3d un_acc_1 = tmp_Q * (linear_acceleration - tmp_Ba) - estimator.g;
+
+    Eigen::Vector3d un_acc = 0.5 * (un_acc_0 + un_acc_1);
+
+    tmp_P = tmp_P + dt * tmp_V + 0.5 * dt * dt * un_acc;
+    tmp_V = tmp_V + dt * un_acc;
+
+    acc_0 = linear_acceleration;
+    gyr_0 = angular_velocity;
+}
+
+void update()
+{
+    TicToc t_predict;
+    latest_time = current_time;
+    tmp_P = estimator.Ps[WINDOW_SIZE];
+    tmp_Q = estimator.Rs[WINDOW_SIZE];
+    tmp_V = estimator.Vs[WINDOW_SIZE];
+    tmp_Ba = estimator.Bas[WINDOW_SIZE];
+    tmp_Bg = estimator.Bgs[WINDOW_SIZE];
+    acc_0 = estimator.acc_0;
+    gyr_0 = estimator.gyr_0;
+
+    queue<sensor_msgs::ImuConstPtr> tmp_imu_buf = imu_buf;
+    for (sensor_msgs::ImuConstPtr tmp_imu_msg; !tmp_imu_buf.empty(); tmp_imu_buf.pop())
+        predict(tmp_imu_buf.front());
+}
+
 // 尾部压入消息
 // 后端位姿
 //todo 这里单独写成一个线程 process_lio
